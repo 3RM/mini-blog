fe8723a59cb7d1d3883bac192eaa247bda9d984c
3RM <rodnoy.mm@gmail.com>
2018-02-03 22:01:31 +0200

    updates

--- template/markup/public/js/scripts.js
+++ template/markup/public/js/scripts.js
@@ -20,7 +20,8 @@
         $('.items').owlCarousel({
             items: 3,
             autoPlay: true,
-            pagination: false
+            pagination: true,
+            autoPlay: 2000,
         });
     }());
 
